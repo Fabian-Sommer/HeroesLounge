3b809a94e4c473c434449f95437a8ac596dadd29
Chris Rogus <rogusdev@gmail.com>
2019-03-14 11:35:04 -0400

    Add overview display title to divisions. Closes #70

---
+++ plugins/rikki/heroeslounge/updates/builder_table_update_rikki_heroeslounge_divisions_2.php
@@ -0,0 +1,23 @@
+<?php namespace Rikki\Heroeslounge\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateRikkiHeroesloungeDivisions2 extends Migration
+{
+    public function up()
+    {
+        Schema::table('rikki_heroeslounge_divisions', function($table)
+        {
+            $table->string('overview_display_title', 255)->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('rikki_heroeslounge_divisions', function($table)
+        {
+            $table->dropColumn('overview_display_title');
+        });
+    }
+}
