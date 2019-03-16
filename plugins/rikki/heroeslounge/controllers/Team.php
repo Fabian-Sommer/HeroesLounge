98ac080017cda3af2ce77daceebfd7cfab7bb7a0
Astraeus- <Astraeus-@users.noreply.github.com>
2019-03-16 16:45:16 +0100

    Refactoring of back-end forms and lists

--- plugins/rikki/heroeslounge/controllers/Team.php
+++ plugins/rikki/heroeslounge/controllers/Team.php
@@ -13,11 +13,10 @@ use Rikki\Heroeslounge\classes\Discord;
 
 class Team extends Controller
 {
-    public $implement = ['Backend\Behaviors\ListController','Backend\Behaviors\FormController','Backend\Behaviors\ReorderController','Backend\Behaviors\RelationController'];
+    public $implement = ['Backend\Behaviors\ListController','Backend\Behaviors\FormController','Backend\Behaviors\RelationController'];
     public $requiredPermissions = ['rikki.heroeslounge.team'];
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
-    public $reorderConfig = 'config_reorder.yaml';
     public $relationConfig = 'config_relation.yaml';
 
     public function __construct()
