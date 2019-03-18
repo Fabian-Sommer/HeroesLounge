48540d1482f7c174c473b682673266a62d57591d
Astraeus- <Astraeus-@users.noreply.github.com>
2019-03-18 23:09:02 +0100

    Clean up backend code

--- plugins/rikki/heroeslounge/controllers/Season.php
+++ plugins/rikki/heroeslounge/controllers/Season.php
@@ -9,11 +9,10 @@ use Rikki\Heroeslounge\Models\Timeline;
 
 class Season extends Controller
 {
-    public $implement = ['Backend\Behaviors\ListController','Backend\Behaviors\FormController','Backend\Behaviors\ReorderController','Backend\Behaviors\RelationController'];
+    public $implement = ['Backend\Behaviors\ListController','Backend\Behaviors\FormController','Backend\Behaviors\RelationController'];
     public $requiredPermissions = ['rikki.heroeslounge.season'];
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
-    public $reorderConfig = 'config_reorder.yaml';
     public $relationConfig = 'config_relation.yaml';
 
     public function __construct()
