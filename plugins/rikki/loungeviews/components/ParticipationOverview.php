a6d3f1ff36fb56b9018b886f7c2c4858d65f6fd9
Fabian Sommer <fabiansbg@web.de>
2019-05-11 18:26:42 +0200

    Allow signing up for a season on season overview page

--- plugins/rikki/loungeviews/components/ParticipationOverview.php
+++ plugins/rikki/loungeviews/components/ParticipationOverview.php
@@ -2,13 +2,16 @@
 
 
 use Cms\Classes\ComponentBase;
+use Auth;
 use Rikki\Heroeslounge\Models\Season as Seasons;
-
 
 class ParticipationOverview extends ComponentBase
 {
     
+    public $user = null;
     public $season = null;
+    public $signedUp = false;
+
     public function componentDetails()
     {
         return [
@@ -21,17 +24,21 @@ class ParticipationOverview extends ComponentBase
 
     public function onRender()
     {
+        $this->user = Auth::getUser();
         $this->season = Seasons::find($this->property('id'));
-        if($this->season)
-        {
+        if ($this->season) {
             $this->page->title = $this->season->title.' Participation';
+            if ($this->user && $this->user->sloth->team && $this->season->teams->contains($this->user->sloth->team)) {
+                $signedUp = true;
+            } else if ($this->user && $this->season->free_agents->contains($this->user->sloth)) {
+                $signedUp = true;
+            }
         }
-
     }
 
     public function defineProperties()
     {
-        return[
+        return [
             'id' => [
                 'title' => 'Entity',
                 'description' => 'The specific Season to take data from',
@@ -49,5 +56,33 @@ class ParticipationOverview extends ComponentBase
             $retOptions[$entity->id] = $entity->title;
         }
         return $retOptions;
+    }
+
+    public function onTeamSignup()
+    {
+        if ($this->user != null) {
+            $team = $this->user->sloth->team;
+            if ($team != null && $this->user->sloth->is_captain && $team->region_id == $this->region_id && !$this->season->teams->contains($team)) {
+                $this->season->teams()->add($team);
+                Flash::success('Your team is now signed up for '.$this->season->title);
+                return Redirect::refresh();
+            }
+        }
+    }
+
+    public function onSlothSignup()
+    {
+        if ($this->user != null) {
+            $sloth = $this->user->sloth;
+            if ($sloth != null && $sloth->team != null) {
+                Flash::error('You cannot be part of a team to sign up as a free agent!');
+                return Redirect::refresh();
+            }
+            if ($sloth != null && $sloth->team == null && $sloth->region_id == $this->region_id && !$this->season->free_agents->contains($sloth)) {
+                $this->season->free_agents()->add($sloth);
+                Flash::success('You are now signed up for '.$this->season->title);
+                return Redirect::refresh();
+            }
+        }
     }
 }
