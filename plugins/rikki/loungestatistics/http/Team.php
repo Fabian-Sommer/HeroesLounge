771df10d1f429a4e3f238d1374c24b74388769f0
Astraeus- <Astraeus-@users.noreply.github.com>
2020-02-18 22:24:19 +0100

    Improve performance for team, hero, and map stats

--- plugins/rikki/loungestatistics/http/Team.php
+++ plugins/rikki/loungestatistics/http/Team.php
@@ -15,27 +15,27 @@ class Team extends Controller
 
     public function herostatistics($id)
     {
-        $team = TeamModel::findOrFail($id);
+        $team = TeamModel::WithMatches()->where('id', $id)->firstOrFail();
         return Statistics::calculateHeroStatisticsForTeam($team, null);
     }
 
     public function seasonHerostatistics($id, $seasonId)
     {
         $season = Season::findOrFail($seasonId);
-        $team = TeamModel::findOrFail($id);
+        $team = TeamModel::WithMatches()->where('id', $id)->firstOrFail();
         return Statistics::calculateHeroStatisticsForTeam($team, $season);
     }
 
     public function mapstatistics($id)
     {
-        $team = TeamModel::findOrFail($id);
+        $team = TeamModel::with('matches', 'matches.games', 'matches.games.map', 'matches.games.replay', 'matches.games.gameParticipations')->where('id', $id)->firstOrFail();
         return Statistics::calculateMapStatisticsForTeam($team, null);
     }
 
     public function seasonMapstatistics($id, $seasonId)
     {
         $season = Season::findOrFail($seasonId);
-        $team = TeamModel::findOrFail($id);
+        $team = TeamModel::with('matches', 'matches.games', 'matches.games.map', 'matches.games.replay', 'matches.games.gameParticipations')->where('id', $id)->firstOrFail();
         return Statistics::calculateMapStatisticsForTeam($team, $season);
     }
 }
