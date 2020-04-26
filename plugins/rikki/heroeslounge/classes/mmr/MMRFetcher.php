8d084e9bf0a2974a5508294483be4699049eb386
Fabian Sommer <fabiansommer1608@gmail.com>
2020-04-26 18:19:54 +0200

    Clean up MMRFetcher

--- plugins/rikki/heroeslounge/classes/mmr/MMRFetcher.php
+++ plugins/rikki/heroeslounge/classes/mmr/MMRFetcher.php
@@ -12,7 +12,7 @@ class MMRFetcher
 
         foreach ($sloths as $sloth) {
             set_time_limit(30);
-            MMRFetcher::updateMMRHeroesProfile($sloth);
+            MMRFetcher::updateMMR($sloth);
         }
     }
 
@@ -22,110 +22,11 @@ class MMRFetcher
 
         foreach ($sloths as $sloth) {
             set_time_limit(30);
-            MMRFetcher::updateMMRHeroesProfile($sloth);
+            MMRFetcher::updateMMR($sloth);
         }
     }
 
     public static function updateMMR($sloth)
-    {
-        $battletag = $sloth->battle_tag;
-        $battletagReformatted = str_replace("#", "_", $battletag);
-
-        $region = "2";
-        if ($sloth->region_id == 2) {
-            $region = "1";
-        }
-        $url = 'https://www.hotslogs.com/API/Players/'.$region.'/'.urlencode($battletagReformatted);
-
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        $output = curl_exec($ch);
-
-        curl_close($ch);
-
-        if ($output == "null") {
-            $urlNA = 'https://www.hotslogs.com/API/Players/1/'.urlencode($battletagReformatted);
-
-            $chNA = curl_init($urlNA);
-            curl_setopt($chNA, CURLOPT_RETURNTRANSFER, true);
-
-            $output = curl_exec($chNA);
-
-            curl_close($chNA);
-        }
-
-        SlothModel::where('id', $sloth->id)->update(['mmr' => 0, 'all_mmr' => 3000]);
-
-        if ($output != "null") {
-            $data = json_decode($output, true);
-
-            if ($data != null) {
-                if (array_key_exists("LeaderboardRankings", $data)) {
-                    $mmrData = $data["LeaderboardRankings"];
-
-                    $mmrs = array();
-                    $mmrs["QuickMatch"] = ["isRanked" => false, "mmr" => -1000];
-                    $mmrs["HeroLeague"] = ["isRanked" => false, "mmr" => -1000];
-                    $mmrs["TeamLeague"] = ["isRanked" => false, "mmr" => -1000];
-                    $mmrs["UnrankedDraft"] = ["isRanked" => false, "mmr" => -1000];
-                    $mmrs["StormLeague"] = ["isRanked" => false, "mmr" => -1000];
-
-                    for ($i = 0; $i < 5; $i++) {
-                        if (array_key_exists($i, $mmrData)) {
-                            $mmrs[$mmrData[$i]["GameMode"]]["isRanked"] = ($mmrData[$i]["LeagueRank"] != null);
-                            $mmrs[$mmrData[$i]["GameMode"]]["mmr"] = $mmrData[$i]["CurrentMMR"];
-                        }
-                    }
-
-                    $slWeight = 70;
-                    $udWeight = 30;
-
-                    $rankedWeight = 0;
-                    $rankedMMR = 0;
-
-                    $allWeight = 0;
-                    $allMMR = 0;
-
-                    if ($mmrs["StormLeague"]["isRanked"]) {
-                        $rankedWeight += $slWeight;
-                        $rankedMMR += $mmrs["StormLeague"]["mmr"] * $slWeight;
-                    }
-                    if ($mmrs["UnrankedDraft"]["isRanked"]) {
-                        $rankedWeight += $udWeight;
-                        $rankedMMR += $mmrs["UnrankedDraft"]["mmr"] * $udWeight;
-                    }
-
-                    if ($mmrs["StormLeague"]["mmr"] != -1000) {
-                        $allWeight += $slWeight;
-                        $allMMR += $mmrs["StormLeague"]["mmr"] * $slWeight;
-                    }
-                    if ($mmrs["UnrankedDraft"]["mmr"] != -1000) {
-                        $allWeight += $udWeight;
-                        $allMMR += $mmrs["UnrankedDraft"]["mmr"] * $udWeight;
-                    }
-
-
-                    if ($allMMR == 0) {
-                        $allMMR += $mmrs["QuickMatch"]["mmr"];
-                        $allWeight = 1;
-                    }
-
-                    if ($rankedWeight > 0) {
-                        $weightedMMR = $rankedMMR/$rankedWeight;
-                        SlothModel::where('id', $sloth->id)->update(['mmr' => $weightedMMR]);
-                    }
-
-                    if ($allWeight > 0) {
-                        $weightedMMR = $allMMR/$allWeight;
-                        SlothModel::where('id', $sloth->id)->update(['all_mmr' => $weightedMMR]);
-                    }
-                }
-            }
-        }
-    }
-
-    public static function updateMMRHeroesProfile($sloth)
     {
         $battletag = $sloth->battle_tag;
         $region = $sloth->getHeroesProfileRegionId();
