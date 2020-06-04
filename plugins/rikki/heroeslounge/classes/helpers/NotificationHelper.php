2ad3ae491eb0d83f836e86f9f59031ca15df508e
Astraeus- <Astraeus-@users.noreply.github.com>
2020-06-04 23:30:10 +0200

    Fix ban notifications

--- plugins/rikki/heroeslounge/classes/helpers/NotificationHelper.php
+++ plugins/rikki/heroeslounge/classes/helpers/NotificationHelper.php
@@ -20,18 +20,17 @@ class NotificationHelper
         $sloth = $user->sloth;
         $retVal = array();
         if (isset($sloth)) {
-            // NotificationHelper::checkHotSLogsProfileStatus($sloth, $retVal);
             NotificationHelper::checkApplications($user, $retVal);
             NotificationHelper::checkParticipation($sloth, $retVal);
 
             if ($sloth->isCaptain()) {
-                NotificationHelper::checkBans($user, $retVal);
+                NotificationHelper::checkBans($retVal);
             }
 
             foreach ($sloth->teams as $key => $team) {
                 if ($sloth->isCaptainOfTeam($team)) {
                     NotificationHelper::checkRoster($team, $retVal);
-                    NotificationHelper::checkMatches($user, $team, $retVal);
+                    NotificationHelper::checkMatches($team, $retVal);
                 }
             }
         }
@@ -46,21 +45,21 @@ class NotificationHelper
                 'type' => 'info',
                 'message' => 'The roster of '. $team->title .' consists of less than 5 members.',
                 'entity' => $team
-                ];
+            ];
         }
     }
 
-    public static function checkMatches($user, $team, &$retVal)
+    public static function checkMatches($team, &$retVal)
     {
-        NotificationHelper::generateScheduleMatchMessages($user->sloth, $team->matches()->whereNull('wbp')->whereNull('is_played')->get(), $retVal);
+        NotificationHelper::generateScheduleMatchMessages($team->matches()->whereNull('wbp')->whereNull('is_played')->get(), $retVal);
         $repMatches = $team->matches()->where(function ($q) {
             $q->whereNotNull('wbp')->where('winner_id', null);
         })->get();
-        NotificationHelper::generateReportMatchMessages($user->sloth, $repMatches, $retVal);
+        NotificationHelper::generateReportMatchMessages($repMatches, $retVal);
     }
 
 
-    private static function generateScheduleMatchMessages($sloth, $matches, &$retVal)
+    private static function generateScheduleMatchMessages($matches, &$retVal)
     {
         if ($matches->count() > 0) {
             $retVal[] = [
@@ -71,7 +70,7 @@ class NotificationHelper
         }
     }
 
-    private static function generateReportMatchMessages($sloth, $matches, &$retVal)
+    private static function generateReportMatchMessages($matches, &$retVal)
     {
         if ($matches->count() > 0) {
             $retVal[] = [
@@ -84,47 +83,45 @@ class NotificationHelper
 
     public static function checkBans(&$retVal)
     {
-        NotificationHelper::generateBanMessages(Bans::with('hero','talent')->get(),$retVal);
+        NotificationHelper::generateBanMessages(Bans::with('hero', 'talent')->get(), $retVal);
     }
 
-    public static function generateBanMessages($bans,&$retVal)
+    public static function generateBanMessages($bans, &$retVal)
     {
         foreach($bans as $ban)
         {
-            $m = 'BANNED: ';
+            $banMessage = 'BANNED: ';
             if(!empty($ban->literal))
             {
-                $m .= $ban->literal;
+                $banMessage .= $ban->literal;
             }
             else
             {
                 if(isset($ban->hero))
                 {
-                    $m .= $ban->hero->title.' ';
+                    $banMessage .= $ban->hero->title.' ';
                 }
                 if(isset($ban->talent))
                 {
-                    $m .= $ban->talent->title.' ';
+                    $banMessage .= $ban->talent->title.' ';
                 }
                 if(isset($ban->round_start))
                 {
-                    $m .= '[Round '.$ban->round_start;
+                    $banMessage .= '[Round '.$ban->round_start;
                     if(isset($ban->round_length))
                     {
-                        $m.= ' - '.($ban->round_start + $ban->round_length);
+                        $banMessage.= ' - '.($ban->round_start + $ban->round_length);
                     }
-                    $m.=' ]';
+                    $banMessage.=' ]';
                 }
             }
             
             $retVal[] = [
                 'type' => 'warning',
-                'message' => $m,
+                'message' => $banMessage,
                 'entity' => $ban->toArray()
-                ];
+            ];
         }
-
-
     }
     
 
@@ -142,21 +139,21 @@ class NotificationHelper
         NotificationHelper::generateApplicationMessages($applicationsToSloth, $user->sloth, $retVal);
     }
 
-    private static function generateApplicationMessages($a, $sloth, &$retVal)
+    private static function generateApplicationMessages($applications, $sloth, &$retVal)
     {
-        foreach ($a as $application) {
+        foreach ($applications as $application) {
             if ($sloth->isCaptainOfTeam($application->team)) {
                 $retVal[] = [
                     'type' => 'info',
                     'message' => $application->sloth->title.' applied for your team!',
                     'entity' => $application->toArray()
-                    ];
+                ];
             } else {
                 $retVal[] = [
                     'type' => 'info',
                     'message' => 'You\'ve recieved an application from '.$application->team->title,
                     'entity' => $application->toArray()
-                    ];
+                ];
             }
         }
     }
@@ -200,17 +197,6 @@ class NotificationHelper
                 'type' => 'warning',
                 'message' => 'You aren\'t listed to participate in '.$season->title.'. You can register as a free agent or join a particpating team.',
                 'entity' => $season->toArray()
-            ];
-        }
-    }
-
-    public static function checkHotSLogsProfileStatus($sloth, &$retVal)
-    {
-        if ($sloth->hotslogs_id == null) {
-            $retVal[]=  [
-                'type' => 'info',
-                'message' => 'Your HotSLogs profile is currently hidden. Please set it to public. This will be required for the next season.',
-                'entity' => $sloth->toArray()
             ];
         }
     }
