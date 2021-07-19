e4f9230cf9c6fbff85b3bc6711382466658439da
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2021-07-19 13:21:49 +0200

    compare with actual season length

--- plugins/rikki/heroeslounge/classes/matchmaking/Swiss.php
+++ plugins/rikki/heroeslounge/classes/matchmaking/Swiss.php
@@ -22,9 +22,9 @@ class Swiss
         $schedule_date = date('Y-m-d H:i:s', strtotime('next sunday 21:55')+$timeoffset);
         $tbp = date('Y-m-d H:i:s', strtotime('+1 weeks sunday 21:55')+$timeoffset);
         
-        // if it's the last round, games must be played until next friday (+5 days)
-        if ($div->season->current_round == 10) {
-	        $tbp = date('Y-m-d H:i:s', strtotime('+5 days sunday 21:55')+$timeoffset);
+        // If it's the last round, games must be played until Friday instead of Sunday.
+        if ($div->season->current_round == $div->season->round_length) {
+	        $tbp = date('Y-m-d H:i:s', strtotime('+1 weeks friday 21:55')+$timeoffset);
         } 
         
         $created_at = date('Y-m-d H:i:s' , strtotime('now'));
