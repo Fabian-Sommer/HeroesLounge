7686635b17dbcf0441dbe67eae5c627dd06c868b
Christoph Staudt <christoph.staudt@uni-jena.de>
2023-02-19 17:20:34 +0100

    Add two new filters for the team stats
    - Allow filtering by only season, excluding all events
    - Allow filtering by the last three seasons
    - Follows a request by Shintaro on Discord

--- plugins/rikki/loungestatistics/components/TeamStatistics.php
+++ plugins/rikki/loungestatistics/components/TeamStatistics.php
@@ -1,24 +1,18 @@
 <?php namespace Rikki\LoungeStatistics\Components;
 
 use Cms\Classes\ComponentBase;
-use Rikki\LoungeStatistics\classes\statistics\Statistics;
-use Rikki\Heroeslounge\Models\Hero;
-use Rikki\Heroeslounge\Models\Team;
 use Rikki\Heroeslounge\Models\Map;
 use Rikki\Heroeslounge\Models\Season;
-use Rikki\Heroeslounge\Models\GameParticipation;
-
-use October\Rain\Support\Collection;
-use Db;
-use Log;
+use Rikki\Heroeslounge\Models\Team;
+use Rikki\LoungeStatistics\classes\statistics\Statistics;
 
 class TeamStatistics extends ComponentBase
 {
     public function componentDetails()
     {
         return [
-            'name'        => 'Team Statistics',
-            'description' => 'Provides HeroesLounge Team Statistics'
+            'name' => 'Team Statistics',
+            'description' => 'Provides HeroesLounge Team Statistics',
         ];
     }
 
@@ -27,6 +21,7 @@ class TeamStatistics extends ComponentBase
     public $maps = null;
     public $participatedSeasons = null;
     public $selectedSeason = null;
+    public $lastThree = null;
 
     public function init()
     {
@@ -38,14 +33,37 @@ class TeamStatistics extends ComponentBase
             ->with('matches', 'matches.division.season', 'matches.games', 'matches.games.replay', 'matches.games.map', 'matches.games.gameParticipations', 'matches.games.gameParticipations.hero', 'matches.games.teamOneFirstBan', 'matches.games.teamOneSecondBan', 'matches.games.teamTwoFirstBan', 'matches.games.teamTwoSecondBan', 'matches.games.teamOneThirdBan', 'matches.games.teamTwoThirdBan')
             ->first();
         $this->participatedSeasons = $this->team->matches
-            ->map(function ($match) { return $match->season; })
-            ->filter(function ($season) { return $season != null; })
-            ->groupBy('id')->map(function ($group) { return $group[0]; })  // unique does not work on these
+            ->map(
+                function ($match) {
+                    return $match->season;
+                }
+            )
+            ->filter(
+                function ($season) {
+                    return $season != null;
+                }
+            )
+            ->groupBy('id')
+            ->map(
+                function ($group) {
+                    return $group[0];
+                }
+            ) // unique does not work on these
             ->sortByDesc('created_at')->values();
 
+        $this->lastThree = $this->participatedSeasons->filter(
+            function ($season) {
+                return in_array($season->type, [1, 2]);
+            }
+        )->
+            take(3)->pluck('title', 'id');
+
         $this->selectedSeason = $this->participatedSeasons
-            ->filter(function ($season) { return $season->is_active; })
-            ->last() ?? $this->participatedSeasons->first();
+            ->filter(
+                function ($season) {
+                    return $season->is_active;
+                }
+            )->last() ?? $this->participatedSeasons->first();
 
         $this->calculateStats($this->selectedSeason);
     }
@@ -53,41 +71,49 @@ class TeamStatistics extends ComponentBase
     public function calculateStats($season)
     {
         $heroes = Statistics::calculateHeroStatisticsForTeam($this->team, $season);
-        $this->heroes = $heroes->sortByDesc(function ($hero_array) {
-            return $hero_array['picks'] + $hero_array['bans_by_team'] + $hero_array['bans_against_team'];
-        });
+        $this->heroes = $heroes->sortByDesc(
+            function ($hero_array) {
+                return $hero_array['picks'] + $hero_array['bans_by_team'] + $hero_array['bans_against_team'];
+            }
+        );
 
         $maps = Statistics::calculateMapStatisticsForTeam($this->team, $season);
-        $this->maps = $maps->sortByDesc(function ($map_array) {
-            return $map_array['picks_by'] + $map_array['picks_vs'];
-        });
+        $this->maps = $maps->sortByDesc(
+            function ($map_array) {
+                return $map_array['picks_by'] + $map_array['picks_vs'];
+            }
+        );
     }
 
     public function onSeasonChange()
     {
         $season_id = input('season_id');
-        if ($season_id == -1) {
+        if ($season_id === 'all-time') {
             $this->selectedSeason = null;
+        } else if ($season_id === 'all-time-real-seasons') {
+            $this->selectedSeason = Season::whereIn('type', [1, 2])->pluck('id')->toArray();
+        } else if ($season_id === 'last-three') {
+            $this->selectedSeason = $this->lastThree->keys()->toArray();
         } else {
             $this->selectedSeason = Season::find($season_id);
         }
         $this->calculateStats($this->selectedSeason);
         return [
-            '#teamstatistics' => $this->renderPartial('@stats')
+            '#teamstatistics' => $this->renderPartial('@stats'),
         ];
     }
 
     public function defineProperties()
     {
-       return [
+        return [
             'team_id' => [
                 'title' => 'Team ID',
                 'description' => 'Team ID to grab data from',
                 'default' => 0,
                 'type' => 'string',
                 'validationPattern' => '^[0-9]+$',
-                'validationMessage' => 'The team_id property can contain only numeric symbols'
-            ]
+                'validationMessage' => 'The team_id property can contain only numeric symbols',
+            ],
         ];
     }
 }
